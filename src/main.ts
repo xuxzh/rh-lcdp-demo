6946ab9adb4c09320304d4a92c51d0ec319b0262
xu_xz <xxz0528@outlook.com>
2023-07-14 13:22:30 +0800

    opt: 首屏添加加载动画

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,32 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
+import { preloaderFinished } from 'rh-base/core';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+preloaderFinished();
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if ((window as any).ngRef) {
+      (window as any).ngRef.destroy();
+    }
+    (window as any).ngRef = ref;
+    // 加载成功之后关闭load页面
+    if ((window as any).appBootstrap) {
+      (window as any).appBootstrap();
+    }
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error(err?.message);
+  })
+  .finally(() => {
+    //
+  });
